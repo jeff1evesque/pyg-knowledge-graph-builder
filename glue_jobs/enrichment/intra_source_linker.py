f3caa4180ef43310c100b9c854e8f24cdae0aa5b
Jeff Levesque <jeff1evesque@yahoo.com>
2026-02-08 15:30:31 -0500

    #30: intra_source_linker.py, implement noaa enrichment

--- glue_jobs/enrichment/intra_source_linker.py
+++ glue_jobs/enrichment/intra_source_linker.py
@@ -6,6 +6,7 @@ from rdflib import Graph
 from glue_jobs.enrichment.intra_source.bls_linker import BLSIntraSourceLinker
 # from glue_jobs.enrichment.intra_source.sec_linker import SECIntraSourceLinker  # Future
 # from glue_jobs.enrichment.intra_source.market_linker import MarketIntraSourceLinker  # Future
+from glue_jobs.enrichment.intra_source.noaa_linker import NOAAIntraSourceLinker
 from typing import Dict
 import logging
 
@@ -29,6 +30,11 @@ def enrich_intra_source(graph: Graph) -> Dict[str, int]:
     logger.info("Starting BLS intra-source enrichment...")
     bls_linker = BLSIntraSourceLinker(graph)
     stats['bls'] = bls_linker.enrich()
+
+    # NOAA enrichment
+    logger.info("\nStarting NOAA intra-source enrichment...")
+    noaa_linker = NOAAIntraSourceLinker(graph)
+    stats['noaa'] = noaa_linker.enrich()
 
     # TODO: Add other source linkers as they're implemented
     # sec_linker = SECIntraSourceLinker(graph)
